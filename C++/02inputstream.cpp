369f5b80ad7402fee082a1a219d6314e69905113
Nilesh Medda <meddanilesh7@gmail.com>
2025-09-17 04:41:38 +0530

    add 02inputstream.cpp and 03summing.cpp

---
+++ C++/02inputstream.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    float b;
+    char c;
+    cout << "Enter an integer, a float, and a character: ";
+    cin >> a >> b >> c;
+    cout<<"you have entered: "<<endl<<"a = "<<a<<endl<<"b = "<<b<<endl<<"c = "<<c<<endl;
+    return 0;
+}
---
+++ C++/03summing.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cout<<"Enter value of three numbers:"<<endl;
+    cin>>a>>b>>c;
+    cout<<"Sum of three numbers is: "<<a+b+c<<endl;
+    return 0;
+}
