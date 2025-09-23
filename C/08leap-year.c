0d3e6e6baf8a00be21886687b379c048e0ccbba2
Nilesh Medda <meddanilesh7@gmail.com>
2025-09-24 03:41:38 +0530

    Add 08leap-year.c file

---
+++ C/08leap-year.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	int year;
+	printf("Enter the year:");
+	scanf("%d", &year);
+	if(year%100==0){
+		if(year%400==0){
+			printf("Year %d is Leap-Year",year);
+		}
+		else{
+			printf("Year %d is not Leap-Year",year);
+		}
+	}
+	else{
+		if(year%4==0){
+			printf("Year %d is Leap-Year",year);
+		}
+		else{
+			printf("Year %d is not Leap-Year",year);
+		}
+	}
+	
+	return 0;
+}
