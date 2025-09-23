d8174770fbe811e800ff1a5f39050a74590b7398
Nilesh Medda <meddanilesh7@gmail.com>
2025-09-23 23:10:56 +0530

    Add 04swap1.c file

---
+++ C/04swap1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	int a, b, c;
+	printf("Enter two numbers: \n");
+	scanf("%d%d", &a, &b);
+	printf ("Before swapping: a=%d and b=%d",a,b);
+	c=a;
+	a=b;
+	b=c;
+	printf("After swapping: a=%d, b=%d\n",a,b);
+
+	return 0;
+}
