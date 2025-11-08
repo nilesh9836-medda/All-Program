991abea7869017c934a47fdefa43989197769c2f
Nilesh Medda <meddanilesh7@gmail.com>
2025-11-08 12:41:03 +0530

    Remove C++, Java, Python folders

--- Java/2.1inputConsole.java
+++
@@ -1,7 +0,0 @@
-class InputConsole {
-    public static void main(String args[]){
-        System.out.println("What is your name: ");
-        String name = System.console().readLine();
-        System.out.println("Your name is : "+name);
-    }
-}
--- Python/03sum.py
+++
@@ -1,4 +0,0 @@
-a=input("Enter first number: ")
-b=input("Enter second number: ")
-c=input("Enter third number: ")
-print("Sum of three numbers is: ", int(a)+int(b)+int(c))
